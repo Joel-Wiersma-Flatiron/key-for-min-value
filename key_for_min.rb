1517105fe22db046a55bfbd4d089b056a398b75c
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-01 14:42:24 -0700

    commit tree 4f0c1767f182c39796306d3fd612f6df2421a509

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_value = ""
+    small = 999
+    name_hash.each do |string, value|
+        if value < small
+            min_value = string
+            small = value
+        end
+    end
+    if min_value == ""
+        return nil
+    else
+        return min_value
+    end
 end
